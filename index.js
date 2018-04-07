6972f8301923d3aa779ddeebc105b97408a26076
Aliaxei Mairyn <alexei.mairyn@gmail.com>
2018-04-07 19:46:22 +0300

    Add files via upload
    Only JS+json
    --------node module - local

---
+++ index.js
@@ -0,0 +1,39 @@
+    let needle = require('needle');
+    let cheerio = require('cheerio');
+    let URL = 'http://www.nbrb.by/statistics/rates/ratesdaily.asp';
+    let json = [],id=0,$;
+    needle.get(URL, function(err, res) {
+        $ = cheerio.load(res.body);
+        $('td.titlecol').each(function (i, element) {
+            let a = $(this);
+            let title = a.text();
+            let countOne= a.next().text();
+            let nameOfValue = a.next().next().text();
+            let currentCourse = a.next().next().next().text();
+            currentCourse = devideValue(currentCourse);
+                        id++;
+                        json.push({
+                            id: id,
+                            valueName:nameOfValue,
+                            name: title,
+                            oneCount:countOne,
+                            currentCourse:currentCourse
+                        });
+                require('fs').writeFileSync('./data.json', JSON.stringify(json, null, 6));
+            });
+    });
+    function devideValue(currentCourse) {
+        let arrayForNewValue = [];
+        let finalValue;
+        let tempCurrent = currentCourse.split("");
+        let currentArray = ['1','2','3','4','5','6','7','8','9','0',','];
+        for(let i=0;i<tempCurrent.length;i++){
+            for(let j=0;j<currentArray.length;j++){
+                if(tempCurrent[i]===currentArray[j]){
+                    arrayForNewValue.push(tempCurrent[i])
+                }
+            }
+        }
+        finalValue = arrayForNewValue.join("");
+        return finalValue;
+    }
